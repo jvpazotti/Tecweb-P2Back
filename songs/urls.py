1cace4f49e30fdcd7dcc4051bdea346e40b44c42
matkwon <kwonmat@outlook.com.br>
2021-11-15 20:37:19 -0300

    back

--- songs/urls.py
+++ songs/urls.py
@@ -3,13 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.index, name='index'),
-    #path('delete',views.delete,name='delete'),
-    #path('edit',views.edit,name='edit'),
-    #path('list_of_tags',views.list_of_tags,name='list_of_tags'),
-    #path('all_tags',views.all_tags,name='all_tags'),
-    #path('api/notes/<int:note_id>/', views.api_note),
-    #path('', views.api_note),
     path('', views.fav_song),
     path('favoritesBack', views.fav_list),
     path('del', views.delete_fav)
--- songs/views.py
+++ songs/views.py
@@ -7,40 +7,31 @@ from .serializers import SongSerializer
 
 # Create your views here.
 
+
 @api_view(['GET', 'POST'])
 def fav_song(request):
     
     song = Song()
 
     if request.method == 'POST':
-        # song = Song()
         print(request.data)
-        song.song_id = int(request.data['song_id'])
+        song.song_id = int(request.data['id'])
         song.save()
-        print("PASSOUUU")
         
     serialized_song = SongSerializer(song)
-
     return Response(serialized_song.data)
 
+
 @api_view(['GET','POST'])
-
 def fav_list(request):
-
-    # if request.method == "POST":
-    #     new_note_data = request.data
-    #     note = Song()
-    #     note.title = new_note_data["title"]
-    #     note.content = new_note_data["content"]
-    #     note.save()
         
     songs = Song.objects.all()
 
     serialized_songs = SongSerializer(songs, many=True)
     return Response(serialized_songs.data)
 
-@api_view(['GET','POST','DELETE'])
 
+@api_view(['GET','POST'])
 def delete_fav(request):
 
     print(request.data)
@@ -51,5 +42,3 @@ def delete_fav(request):
         all_songs = Song.objects.all()
         serialized_song = SongSerializer(all_songs, many=True)
     return Response(serialized_song.data)
-
-
