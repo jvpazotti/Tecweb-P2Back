2d53edd787358163b2fec07612a0929403e73f51
matkwon <kwonmat@outlook.com.br>
2021-11-15 23:28:57 -0300

    unfav

--- songs/urls.py
+++ songs/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.fav_song),
     path('favoritesBack', views.fav_list),
-    path('homeDel', views.unfav),
+    path('searchDel', views.unfav),
     path('del', views.delete_fav)
 ]
